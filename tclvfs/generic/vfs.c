21118fde4804ea18647bf0ae370f99756d17c674
hobbs <>
2002-09-26 02:12:47 +0000

    * Makefile.in:               replace the ancient build system 	* README.cygwin:             with TEA 2002 build spec. 	* aclocal.m4: 	* config.guess (removed): 	* config.sub (removed): 	* configure: 	* configure.in: 	* install-sh (removed): 	* mkIndex.tcl.in (removed): 	* mkinstalldirs (removed): 	* tcl.m4 (removed): 	* tclconfig/README.txt (new): 	* tclconfig/install-sh (new): 	* tclconfig/tcl.m4 (new): 	* generic/vfs.c: changed BUILD_Vfs to BUILD_vfs

--- tclvfs/generic/vfs.c
+++ tclvfs/generic/vfs.c
@@ -25,10 +25,10 @@
 
 /*
  * Windows needs to know which symbols to export.  Unix does not.
- * BUILD_Vfs should be undefined for Unix.
+ * BUILD_vfs should be undefined for Unix.
  */
 
-#ifdef BUILD_Vfs
+#ifdef BUILD_vfs
 #undef TCL_STORAGE_CLASS
 #define TCL_STORAGE_CLASS DLLEXPORT
 #endif /* BUILD_Vfs */
