b0659a7db8e4eb05b55182a869a1ca92dff9da85
vincentdarley <>
2003-05-14 18:29:14 +0000

    vfs library fix

--- tclvfs/library/vfsUtils.tcl
+++ tclvfs/library/vfsUtils.tcl
@@ -127,14 +127,14 @@ proc vfs::attributeCantConfigure {attr val largs} {
     }
 }
 
-::vfs::autoMountExtension "" ::vfs::mk4::Mount vfs
-::vfs::autoMountExtension .bin ::vfs::mk4::Mount vfs
-::vfs::autoMountExtension .kit ::vfs::mk4::Mount vfs
-::vfs::autoMountExtension .tar ::vfs::tar::Mount vfs
-::vfs::autoMountExtension .zip ::vfs::zip::Mount vfs
-::vfs::autoMountUrl ftp ::vfs::ftp::Mount vfs
+::vfs::autoMountExtension "" ::vfs::mk4::Mount vfs::mk4
+::vfs::autoMountExtension .bin ::vfs::mk4::Mount vfs::mk4
+::vfs::autoMountExtension .kit ::vfs::mk4::Mount vfs::mk4
+::vfs::autoMountExtension .tar ::vfs::tar::Mount vfs::tar
+::vfs::autoMountExtension .zip ::vfs::zip::Mount vfs::zip
+::vfs::autoMountUrl ftp ::vfs::ftp::Mount vfs::ftp
 ::vfs::autoMountUrl file ::vfs::fileUrlMount vfs
-::vfs::autoMountUrl tclns ::vfs::tclprocMount vfs
+::vfs::autoMountUrl tclns ::vfs::tclprocMount vfs::ns
 
 proc ::vfs::haveMount {url} {
     variable mounted
