a86a16ecee644afc39fb0572aad809c6a0498996
das <>
2004-05-21 10:45:27 +0000

    * library/pkgIndex.tcl.in: brought back into sync with pkgIndex.tcl 	* library/pkgIndex.tcl: fixed spacing to be like pkgIndex.tcl.in

--- tclvfs/library/pkgIndex.tcl
+++ tclvfs/library/pkgIndex.tcl
@@ -44,22 +44,22 @@ proc loadvfs {dir dll} {
     load $dll
 }
 
-package ifneeded vfs 1.3.0 [list loadvfs $dir $dll]
-package ifneeded starkit 1.3.1 [list source [file join $dir starkit.tcl]]
-package ifneeded vfslib 1.3.1 [list source [file join $dir vfslib.tcl]]
+package ifneeded vfs        1.3.0 [list loadvfs $dir $dll]
+package ifneeded starkit    1.3.1 [list source [file join $dir starkit.tcl]]
+package ifneeded vfslib     1.3.1 [list source [file join $dir vfslib.tcl]]
 
 # Old
-package ifneeded mk4vfs 1.9 [list source [file join $dir mk4vfs.tcl]]
-package ifneeded zipvfs 1.0 [list source [file join $dir zipvfs.tcl]]
+package ifneeded mk4vfs       1.9 [list source [file join $dir mk4vfs.tcl]]
+package ifneeded zipvfs       1.0 [list source [file join $dir zipvfs.tcl]]
 
 # New
-package ifneeded vfs::ftp 1.0 [list source [file join $dir ftpvfs.tcl]]
-package ifneeded vfs::http 0.5 [list source [file join $dir httpvfs.tcl]]
-package ifneeded vfs::mk4 1.9 [list source [file join $dir mk4vfs.tcl]]
-package ifneeded vfs::ns 0.5 [list source [file join $dir tclprocvfs.tcl]]
-package ifneeded vfs::tar 0.9 [list source [file join $dir tarvfs.tcl]]
-package ifneeded vfs::test 1.0 [list source [file join $dir testvfs.tcl]]
+package ifneeded vfs::ftp     1.0 [list source [file join $dir ftpvfs.tcl]]
+package ifneeded vfs::http    0.5 [list source [file join $dir httpvfs.tcl]]
+package ifneeded vfs::mk4     1.9 [list source [file join $dir mk4vfs.tcl]]
+package ifneeded vfs::ns      0.5 [list source [file join $dir tclprocvfs.tcl]]
+package ifneeded vfs::tar     0.9 [list source [file join $dir tarvfs.tcl]]
+package ifneeded vfs::test    1.0 [list source [file join $dir testvfs.tcl]]
 package ifneeded vfs::urltype 1.0 [list source [file join $dir vfsUrl.tcl]]
-package ifneeded vfs::webdav 0.1 [list source [file join $dir webdavvfs.tcl]]
-package ifneeded vfs::zip 1.0 [list source [file join $dir zipvfs.tcl]]
-package ifneeded vfs::tk 0.5 [list source [file join $dir tkvfs.tcl]]
+package ifneeded vfs::webdav  0.1 [list source [file join $dir webdavvfs.tcl]]
+package ifneeded vfs::zip     1.0 [list source [file join $dir zipvfs.tcl]]
+package ifneeded vfs::tk      0.5 [list source [file join $dir tkvfs.tcl]]
